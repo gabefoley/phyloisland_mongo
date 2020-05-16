6165ca5f664a0f1bb3aeb51f5594525d74fbfa91
gabefoley <gabriel.foley@uqconnect.edu.au>
2020-05-16 17:39:43 +1000

    Added Featue Log

--- views.py
+++ views.py
@@ -1254,6 +1254,12 @@ class ProfileView(ModelView):
         exec('@action("' + name + '_set_reference", "Set this profile as the ' + name + ' reference")\ndef ' + name +
              '_set_reference(self, profile_ids):\n utilities.set_profile_as_reference(profile_ids, "' + name + '")')
 
+class FeatureLogView(BaseView):
+    @expose("/")
+    def feature_log(self):
+        return self.render('features.html')
+
+
 
 class DocumentationView(BaseView):
     @expose("/")
@@ -1674,5 +1680,5 @@ with warnings.catch_warnings():
     admin.add_view(DownloadFastaView(name='Download FASTA', endpoint='download_fasta'))
     # admin.add_view(DownloadGenomeOrderView(name='Download genome order', endpoint='download_order'))
     # admin.add_view(TempFixView(name='Temp Fix', endpoint='temp_fix'))
-
+    admin.add_view(FeatureLogView(name='Feature Log', endpoint='features'))
     admin.add_view(DocumentationView(name='Documentation & FAQ', endpoint='documentation'))
